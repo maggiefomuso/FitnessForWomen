c316874f70f44ab7b5183469b00b1ce114e58a45
maggiefomuso <maggie_fomuso@yahoo.fr>
2015-12-15 17:10:35 +0100

    creation of 2nd activity for freetrail

--- app/src/main/java/com/students/mics/fitnessforwomen/MainActivity.java
+++ app/src/main/java/com/students/mics/fitnessforwomen/MainActivity.java
@@ -1,18 +1,28 @@
 package com.students.mics.fitnessforwomen;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button bt = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
+        bt  = (Button)findViewById(R.id.buttonFreeTrail);
+        bt.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent call = new Intent(getApplicationContext(), FTSelection.class);
+                startActivity(call);
+            }
+        });
     }
 
     @Override
